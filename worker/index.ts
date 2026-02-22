4a14bef49f9dad4760024195a1b57ecc9e5b9281
Jacob Weisz <inbox@jacobweisz.com>
2026-02-22 16:50:42 -0600

    Browsers should download it

--- worker/index.ts
+++ worker/index.ts
@@ -3,6 +3,6 @@ import { env, WorkerEntrypoint } from "cloudflare:workers";
 export default class extends WorkerEntrypoint {
   async fetch(request: Request) {
     const installScript = await env.ASSETS.fetch("https://install.sandstorm.org/install.sh");
-    return new Response(installScript.body, {status: 200, headers: {"Content-Type": "text/x-shellscript",},});
+    return new Response(installScript.body, {status: 200, headers: {"Content-Type": "text/x-shellscript", "Content-Disposition": 'attachment; filename="install.sh"',},});
   }
 }
